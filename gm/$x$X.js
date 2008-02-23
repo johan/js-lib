99bf9d7c61a421292a916dfb5aa276fcf5a9b392
Johan Sundström <oyasumi@gmail.com>
2008-02-23 01:26:58 +0000

    It's actually better not to even expose the type option; it making code both larger and less readable, while preventing a third nsresolver parameter.
    git-svn-id: https://ecmanaut.googlecode.com/svn/trunk/lib@111 c5938665-bd24-0410-9958-d597d88377d4

--- gm/$x$X.js
+++ gm/$x$X.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name           $X and $x: XPath shorthand API
 // @namespace      http://code.google.com/p/ecmanaut/
-// @description    Evaluates an XPath expression, returning the first ($X) / all ($x) nodes matching the query. If the result is a string, number or boolean, return the corresponding javascript basic type. The root parameter is an optional context node relative to which to resolve the expression, type optionally sets a different XPath resolving type than XPathResult.ANY_TYPE, if you want to optimize extensively.
+// @description    Evaluates an XPath expression, returning the first ($X) / all ($x) nodes matching the query. If the result is a string, number or boolean, return the corresponding javascript basic type. The root parameter is an optional context node relative to which to resolve the expression.
 // ==/UserScript==
 
 // list nodes matching this expression, optionally relative to the node `root'
-function $x( xpath, root, type ) {
+function $x( xpath, root ) {
   var doc = root ? root.evaluate ? root : root.ownerDocument : document, next;
-  var got = doc.evaluate( xpath, root||doc, null, type||0, null ), result = [];
+  var got = doc.evaluate( xpath, root||doc, null, 0, null ), result = [];
   switch (got.resultType) {
     case got.STRING_TYPE:
       return got.stringValue;
@@ -22,7 +22,7 @@ function $x( xpath, root, type ) {
   }
 }
 
-function $X( xpath, root, type ) {
-  var got = $x( xpath, root, type );
+function $X( xpath, root ) {
+  var got = $x( xpath, root );
   return got instanceof Array ? got[0] : got;
 }
