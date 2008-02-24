82b8e6ed7a73261fbbb41c32e9bc305d51b5e9d3
Johan Sundström <oyasumi@gmail.com>
2008-02-24 23:08:14 +0000

    Forgot this part of the microformat. How glad am I not that I only have to update this one spot to mend ten(?) user scripts out there?
    git-svn-id: https://ecmanaut.googlecode.com/svn/trunk/lib@134 c5938665-bd24-0410-9958-d597d88377d4

--- gm/unpaginate.js
+++ gm/unpaginate.js
@@ -3,18 +3,26 @@ function unpaginate(items, next, pane) {
   function count(xpath) {
     return $X("count("+ xpath +")");
   }
+  function arrayify(string) {
+    if (typeof string == "string")
+      return [string];
+    return string;
+  }
 
-  if (typeof items == "string")
-    items = [items];
-  items = items.filter(count).shift();
+  items = arrayify(items).filter(count).shift();
 
   var a = $X(next);
   if (a || items) {
     //console.info("producing %x", location.href);
     addMeta("items-xpath", items);
-    if (typeof $X(pane) == "object")
-      addMeta("pagination-container", pane);
-    if (a.href)
+
+    arrayify(pane).filter(count).forEach(function(xpath) {
+      addMeta("pagination-container", xpath);
+    });
+
+    if (a.href) {
       addLink("next", a.href);
+      addMeta("next-xpath", next);
+    }
   }
 }
