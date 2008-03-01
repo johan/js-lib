3826816d904d81c2d6fc8a7277f161f35c18d5c6
Johan Sundström <oyasumi@gmail.com>
2008-03-01 12:37:45 +0000

    Whitespace cleanup. :-)
    git-svn-id: https://ecmanaut.googlecode.com/svn/trunk/lib@148 c5938665-bd24-0410-9958-d597d88377d4

--- jsonp.js
+++ jsonp.js
@@ -4,11 +4,11 @@ var JSONP = {
   err:{},
 
   load:function(url, cb, staticCallbackName) {
-    function cleanup( failedURL ) {
+    function cleanup(failedURL) {
       try {
 	delete JSONP.cb[name];
 	delete JSONP.err[name];
-	if( script && script.parentNode )
+	if (script && script.parentNode)
 	  script.parentNode.removeChild(script);
       } finally {
 	failedURL && cb && cb(undefined, failedURL);
@@ -17,12 +17,12 @@ var JSONP = {
 
     var n = ++JSONP.n;
     var sURL = url;
-    if( !staticCallbackName ) {
-      if( -1 == sURL.indexOf("?") )
+    if (!staticCallbackName) {
+      if (-1 == sURL.indexOf("?"))
 	sURL += "?";
       var last = /.$/.exec( sURL )[0];
-      if( last != "=") {
-        if( last != '&' && last != "?")
+      if (last != "=") {
+        if (last != '&' && last != "?")
 	  sURL += "&";
         sURL += 'callback=';
       }
@@ -36,7 +36,7 @@ var JSONP = {
     };
 
     var full = "JSONP.cb." + name;
-    if( staticCallbackName )
+    if (staticCallbackName)
       eval( staticCallbackName +"="+ full );
     sURL += full;
 
@@ -45,18 +45,18 @@ var JSONP = {
     return JSONP.cb[name];
   },
 
-  script:function( doc, url, err ) {
+  script:function(doc, url, err) {
     err = err || "";
-    if(!doc.body)
+    if (!doc.body)
       try {
 	var q = "'"+ err +"'";
         doc.write("<script src='"+ url +"' err="+ q +
 		  " type='text/javascript'>"+ err + "</script>");
         return;
-      }catch(e){}
+      } catch(e) {}
 
     var script = doc.createElement("script");
-    if( err ) {
+    if (err) {
       script.innerHTML = err;
       script.setAttribute("onerror", err);
     }
